a9f8a904e27ba4969021c997eb23f9e5e0185cd1
Kenny <kennypicavet10@gmail.com>
2026-05-05 20:55:44 +0200

    test

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,10 @@
-import { ApplicationConfig, inject, provideAppInitializer, provideZonelessChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZonelessChangeDetection } from '@angular/core';
 import { getApp, provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import {
-  Auth,
   browserSessionPersistence,
-  getAuth,
+  initializeAuth,
   provideAuth,
-  setPersistence,
 } from '@angular/fire/auth';
 import { provideFunctions, getFunctions } from '@angular/fire/functions';
 import { environment } from '../environments/environment';
@@ -15,14 +13,12 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZonelessChangeDetection(),
     provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAuth(() => getAuth()),
+    provideAuth(() =>
+      initializeAuth(getApp(), {
+        persistence: browserSessionPersistence,
+      }),
+    ),
     provideFirestore(() => getFirestore()),
     provideFunctions(() => getFunctions(getApp(), 'europe-west1')),
-    provideAppInitializer(() => {
-      const auth = inject(Auth);
-      return setPersistence(auth, browserSessionPersistence).catch((err) => {
-        console.error('Persistence setup mislukt:', err);
-      });
-    }),
   ],
 };
